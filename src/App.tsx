3a7aef37ec17316278146d45d6f0707d786123a6
Mohsin Tabba Ovais <ahme.mohsin@gmail.com>
2024-06-29 21:14:27 +0500

    updated with vite react tailwind

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1 className="text=3xl from-neutral-200 font-thin">
+      Boiler Plate React+Vite + Tailwind 
+    </h1>
+    <div className="card">
+      <button onClick={() => setCount((count) => count + 1)}>
+          Increment 
+      </button>
+      <h1>{count}</h1>
+      <button onClick={() => setCount((count) => count - 1)}>
+          Decrement
+      </button>
+    </div>
+    </>
+  )
+}
+
+export default App
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
-module.exports = {
-  purge: [],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  variants: {},
   plugins: [],
 }
+
